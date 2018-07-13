cd031b3f272817eca14522e35024a89eee4f479d
DatavenueLiveObjects <liveobjects.contact@orange.com>
2018-07-13 16:29:35 +0200

    Class FIFO Objects

--- Data management/Publish_consume_data_in_FIFO_mode/jsonFIFO/LoSampleFIFO.py
+++ Data management/Publish_consume_data_in_FIFO_mode/jsonFIFO/LoSampleFIFO.py
@@ -2,20 +2,20 @@ import numpy as np
 
 class Location():
     """docstring for Location."""
-    def __init__(self, arg):
+    def __init__(self):
         self.lat = 0.0
         self.lon = 0.0
 
 class SampleData():
     """docstring for SampleData."""
-    def __init__(self, arg):
+    def __init__(self):
         self.payload = ""
         self.temp = 0
 
 
 class LoFifoData(Location):
     """docstring for ."""
-    def __init__(self, arg):
+    def __init__(self):
         # super(Location, self).__init__()
         self.location = Location()
         self.v = SampleData()
