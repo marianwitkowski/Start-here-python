18716692fc3c328dc239e9b51a1812071318c169
DatavenueLiveObjects <liveobjects.contact@orange.com>
2018-07-13 16:38:17 +0200

    update directory name

---
+++ Data management/Publish_Consume_Data_in_PubSub_Mode/PubSubConsumer.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+
+SERVER = "liveobjects.orange-business.com"
+PORT = 1883
+API_KEY   = "Your API_KEY"
+USERNAME  = "payload+bridge"
+CLIENT_ID = "myFifoClientId"
+
+#Publications parameters
+TOPIC="pubsub/pubSubSampleTopic" # topic to publish to
+qos = 1
+KEEP_ALIVE_INTERVAL = 30 # Must be <= 50
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected in bridge mode with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe(TOPIC)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(sampleClient, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+def on_subscribe(sampleClient, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+print("Runinng consumer appId=" + CLIENT_ID)
+sampleClient = mqtt.Client(CLIENT_ID, clean_session=True, userdata = None, protocol=mqtt.MQTTv311, transport="tcp")
+sampleClient.on_connect = on_connect
+sampleClient.on_message = on_message
+sampleClient.on_subscribe = on_subscribe
+sampleClient.username_pw_set(USERNAME,password = list(API_KEY)) # use device mode and set the password
+sampleClient.reconnect_delay_set(min_delay=1, max_delay=120)
+
+# now connect to LO
+print "Connecting to broker:", SERVER
+sampleClient.connect(SERVER, PORT, KEEP_ALIVE_INTERVAL)
+print "... connected."
+
+sampleClient.subscribe(TOPIC,qos)
+sampleClient.loop_forever()
---
+++ Data management/Publish_Consume_Data_in_PubSub_Mode/PubSubPublisher.py
@@ -0,0 +1,43 @@
+import paho.mqtt.client as mqtt
+from datetime import datetime
+
+
+SERVER = "liveobjects.orange-business.com"
+PORT = 1883
+API_KEY   = "Your API_KEY"
+USERNAME  = "payload+bridge"
+CLIENT_ID = "myFifoClientId"
+
+#Publications parameters
+TOPIC="pubsub/pubSubSampleTopic" # topic to publish to
+qos = 1
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected in bridge mode with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe(TOPIC)
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(sampleClient, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+# Create and fill your connections options
+sampleClient = mqtt.Client(CLIENT_ID, clean_session=True, userdata = None, protocol=mqtt.MQTTv311, transport="tcp")
+sampleClient.on_connect = on_connect
+sampleClient.on_message = on_message
+sampleClient.username_pw_set(USERNAME,password = list(API_KEY)) # use device mode and set the password
+# now connect to LO
+sampleClient.connect(SERVER, PORT, 60)
+
+# send your message to the topic
+msg = "Message published on pubsub topic on "+datetime.now().isoformat()
+
+sampleClient.loop_start()
+sampleClient.publish(TOPIC, msg, qos)
+print ("Message published")
+
+# Disconnect
+sampleClient.disconnect()
+print("Disconnected")
