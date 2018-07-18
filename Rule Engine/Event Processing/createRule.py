15656bba6d1f07575678d9d61c3d77618044f076
DatavenueLiveObjects <liveobjects.contact@orange.com>
2018-07-18 10:52:03 +0200

    update credentials

--- Rule Engine/Event Processing/createRule.py
+++ Rule Engine/Event Processing/createRule.py
@@ -1,8 +1,8 @@
 class createRule(object):
     """docstring for createRule."""
     SERVER = "https://liveobjects.orange-business.com/api/v0/"
-    API_KEY = "f6422939005946619dd8a2575db3cb76"
-    EMAIL = "zafnuxli@gmail.com"
+    API_KEY = "YOUR API_KEY"
+    EMAIL = "mail@gmail.com"
 
     def __init__(self, arg):
         super(createRule, self).__init__()
@@ -12,7 +12,7 @@ class createRule(object):
     def createMatchingRule():
         matchingRuleId = ""
         serial="{\"dataPredicate\":{\"<\": [ {\"var\":\"value.hygrometry\"},20]},\"enabled\":true,\"name\": \"Test1211\"}";
-        
+
     def main():
         matchingRuleIds = createMatchingRule()
         firingRuleId = createFiringRule(matchingRuleId)
